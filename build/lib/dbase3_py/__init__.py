51094b850a16c1b03ea73e4a958cd559c9ca63c1
sandy98 <esavoretti@gmail.com>
2024-12-26 12:05:28 -0300

    v0.1.15

--- build/lib/dbase3_py/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = "0.1.13"
-__author__ = "Domingo E. Savoretti"
-__email__ = "esavoretti@gmail.com"
-__license__ = "MIT"
-__url__ = "https://github.com/sandy98/dbase3-py"
-__description__ = "A simple library to read and write dbase III files."
